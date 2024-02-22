183c6416f4fb88de8ce3589fe4481d16c9815da0
SkaiLord <skailord007@gmail.com>
2024-02-22 16:20:22 +0530

    project page fix

--- src/components/Projects/SDG.jsx
+++ src/components/Projects/SDG.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 
 function SDG(props){
-    return(
-        <div className='sdg'>
-              <img className="sdg-img" src={props.img}></img>
-            <div className='sdg-text'>
-              <p className='sdg-head'>{props.head}</p>
-              <p className='sdg-subhead'>{props.subhead}</p>
-              <div className='sdg-content-box'>
-                <p className='sdg-content'>{props.content}</p>
-              </div>
-            </div>
-            {/* <div className='sdg-images'> */}
-              {/* <img className="sdg-overlap" style={} src={}></img> */}
-            {/* </div> */}
+    return (
+      <div className="sdg">
+        <img className="sdg-img" src={props.img} alt="sdg" />
+        <div className="sdg-text">
+          <div className="sdg-head">{props.head}</div>
+          <div className="sdg-subhead">{props.subhead}</div>
+          <div className="sdg-content-box">
+            <div className="sdg-content">{props.content}</div>
           </div>
-    )
+        </div>
+        {/* <div className='sdg-images'> */}
+        {/* <img className="sdg-overlap" style={} src={}></img> */}
+        {/* </div> */}
+      </div>
+    );
 }
 
 export default SDG;
